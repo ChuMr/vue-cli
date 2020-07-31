138ec9dba40c9589bc45df5bd7807b051cea2577
Qian Zhang <qian.zhang@edaili.com>
2020-07-31 18:04:14 +0800

    first commit

---
+++ src/views/main.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-container>
+    <el-header>
+      <div class="m-header">
+        <div class="nav-link">
+          <ul>
+            <li>主站</li>
+            <li>个人导航</li>
+            <li>文档系统</li>
+            <li>B博</li>
+            <li>下载APP</li>
+          </ul>
+        </div>
+        <div>
+          <input value="输入你想搜索的内容">
+        </div>
+        <div>
+          <span>登录</span>
+          <span>注册</span>
+        </div>
+      </div>
+    </el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+<style>
+  .el-header {
+    background-image: url("https://img.xiaoyou66.com/images/2020/02/06/npBl.png");
+    min-height: 155px;
+    background-size: cover;
+    /*background-size: contain;*/
+  }
+  .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-main {
+    background-image: url("https://img.xiaoyou66.com/images/2020/02/06/tKb5.jpg");
+    min-height: 1000px;
+    background-size: cover;
+    /*background-color: #E9EEF3;*/
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+</style>
+<script>
+
+</script>
